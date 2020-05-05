655ccb1857ec41ea14f588fca8199b67ebfb2d0d
Mirko <mirko96@gmx.ch>
2020-05-05 19:21:14 +0200

    Revert "test"
    This reverts commit 5e843c67213cf5377222ea4e435fe9801e4cb5c4.

--- train.py
+++ train.py
@@ -160,29 +160,31 @@ if __name__ == "__main__":
             # ----------------
 
             log_str = "\n---- [Epoch %d/%d, Batch %d/%d] ----\n" % (epoch, opt.epochs, batch_i, len(dataloader))
+            if opt.metric:
+                metric_table = [["Metrics", *[f"YOLO Layer {i}" for i in range(len(model.yolo_layers))]]]
 
-            metric_table = [["Metrics", *[f"YOLO Layer {i}" for i in range(len(model.yolo_layers))]]]
-
-            # Log metrics at each YOLO layer
-            for i, metric in enumerate(metrics):
-                formats = {m: "%.6f" for m in metrics}
-                formats["grid_size"] = "%2d"
-                formats["cls_acc"] = "%.2f%%"
-                row_metrics = [formats[metric] % yolo.metrics.get(metric, 0) for yolo in model.yolo_layers]
-                metric_table += [[metric, *row_metrics]]
+                # Log metrics at each YOLO layer
+                for i, metric in enumerate(metrics):
+                    formats = {m: "%.6f" for m in metrics}
+                    formats["grid_size"] = "%2d"
+                    formats["cls_acc"] = "%.2f%%"
+                    row_metrics = [formats[metric] % yolo.metrics.get(metric, 0) for yolo in model.yolo_layers]
+                    metric_table += [[metric, *row_metrics]]
 
                 # Tensorboard logging
-                if opt.logger:
-                    tensorboard_log = []
-                    for j, yolo in enumerate(model.yolo_layers):
-                        for name, metric in yolo.metrics.items():
-                            if name != "grid_size":
-                                tensorboard_log += [(f"{name}_{j + 1}", metric)]
-                    tensorboard_log += [("loss", loss.item())]
-                    logger.list_of_scalars_summary(tensorboard_log, batches_done)
+                    if opt.logger:
+                        tensorboard_log = []
+                        for j, yolo in enumerate(model.yolo_layers):
+                            for name, metric in yolo.metrics.items():
+                                if name != "grid_size":
+                                    tensorboard_log += [(f"{name}_{j+1}", metric)]
+                        tensorboard_log += [("loss", loss.item())]
+                        logger.list_of_scalars_summary(tensorboard_log, batches_done)
+                if batch_i % log_every == 0:  # td change batch_i
 
                 log_str += AsciiTable(metric_table).table
-                log_str += f"\nTotal loss {loss.item()}"
+
+            log_str += f"\nTotal loss {loss.item()}"
 
             # Determine approximate time left for epoch
             epoch_batches_left = len(dataloader) - (batch_i + 1)
