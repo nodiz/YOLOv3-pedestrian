8acffcb4ac75c39bf00c3e81e693ed6cd9f0dc7e
Niccolò <ssnnca@gmail.com>
2020-05-05 23:14:02 +0200

    datetime incongruence

--- utils/logger_torch.py
+++ utils/logger_torch.py
@@ -1,12 +1,12 @@
 from torch.utils.tensorboard import SummaryWriter
-from datetime import datetime
+import datetime
 import torchvision
 
 
 class Logger(object):
     def __init__(self, log_dir):
         """Create a summary writer logging to log_dir."""
-        now = datetime.now()
+        now = datetime.datetime.now()
         dt_string = now.strftime("%d%H%M")
         self.writer = SummaryWriter(log_dir, comment=dt_string)
 
